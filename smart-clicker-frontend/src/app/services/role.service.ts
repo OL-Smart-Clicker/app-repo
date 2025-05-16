506d0afe355a6e15a9818d88e731f35e093bddcf
Andrei Niculescu <a.niculescu@student.fontys.nl>
2025-05-16 11:30:12 +0200

    added dev proxy config, changed api calls to /api/*

--- smart-clicker-frontend/src/app/services/role.service.ts
+++ smart-clicker-frontend/src/app/services/role.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { env } from '../../environments/environment';
 import axios from 'axios';
 import { AuthService } from './auth.service';
 import { Role } from '../types/role';
@@ -20,7 +19,7 @@ export class RoleService {
                 Authorization: `Bearer ${token}`
             }
         };
-        const response = await axios.get(`${env.API_URL}/role/${id}`, config)
+        const response = await axios.get(`/api/role/${id}`, config)
         return response.data as Role;
     }
 }
